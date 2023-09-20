4b7ff87a69fb7a1d9c01089444580d1bc6504c0f
NISCHAY3 <118152587+NISCHAY3@users.noreply.github.com>
2023-09-20 16:53:36 +0530

    day10

---
+++ Day10/maxOccuranceChar.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+char getMaxOcc(string s)
+{
+    int arr[26] = {0};
+    for (int i = 0; i < s.length(); i++)
+    {
+        char ch = s[i];
+
+        int number = 0;
+        number = ch - 'a';
+        arr[number]++;
+    }
+
+    int maxi = -1;
+    int ans = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (maxi < arr[i])
+        {
+            ans = i;
+            maxi = arr[i];
+        }
+    }
+    return 'a' + ans;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    cout << getMaxOcc(s) << endl;
+
+    return 0;
+}
