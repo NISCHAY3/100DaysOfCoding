009df8990e2e0bae53e709c1a7c267c35169d21b
NISCHAY3 <118152587+NISCHAY3@users.noreply.github.com>
2023-10-21 14:45:24 +0530

    day41

---
+++ Day41/Queues.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    cout << "size of queue is :" << q.size() << endl;
+
+    if (q.empty())
+    {
+        cout << "queue is empty" << endl;
+    }
+    else
+    {
+        cout << "queue is not empty" << endl;
+    }
+
+    cout << "front element is :" << q.front() << endl;
+
+    return 0;
+}
