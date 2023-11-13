4aa70e6ebfcb10981094403f914a90a06c4fd45f
NISCHAY3 <118152587+NISCHAY3@users.noreply.github.com>
2023-11-13 17:35:51 +0530

    day64

---
+++ Day64/validateBST.cpp
@@ -0,0 +1,24 @@
+// Question Link->https://www.codingninjas.com/studio/problems/validate-bst_981275?leftPanelTabValue=SUBMISSION
+
+bool isBST(BinaryTreeNode<int>* root,int min,int max){
+
+    if(root==NULL){
+        return true;
+    }
+    if(root->data>min && root->data<max){
+
+        bool left=isBST(root->left,min,root->data);
+        bool right=isBST(root->right,root->data,max);
+        return left && right;
+
+    }
+    else{
+        return false;
+    }
+
+}
+bool validateBST(BinaryTreeNode<int>* root) 
+{
+    return isBST(root,INT_MIN,INT_MAX);
+
+}
